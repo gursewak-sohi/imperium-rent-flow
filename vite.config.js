cb3285a2a88bdc33bb7c35dffcb61b6c35c1a72b
gursewak <gursewak@gursewaks-MacBook-Pro.local>
2022-09-23 04:11:08 +0530

    base change

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/deploy-impjets/',
     plugins: [vue()],
-    "base": "/deploy-impjets/",
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src',
-                import.meta.url))
-        }
-    }
 })
