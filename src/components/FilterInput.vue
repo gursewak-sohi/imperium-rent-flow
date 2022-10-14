c55e98382bc287be06267b076521e69ad316d24f
teqdeft <teqdeft@gmail.com>
2022-10-14 13:08:49 +0530

    fixes

--- src/components/FilterInput.vue
+++ src/components/FilterInput.vue
@@ -195,7 +195,7 @@
         var d = date.getDate();
         var m = date.getMonth() + 1; //Month from 0 to 11
         var y = date.getFullYear();
-        return '' + (m<=9 ? '0' + m : m) + (d <= 9 ? '0' + d : d) + '/' + '/'   + y ;
+        return '' + (m<=9 ? '0' + m : m) + '/' + (d <= 9 ? '0' + d : d)  + '/'   + y ;
     }
 
     setTimeout(function(){
